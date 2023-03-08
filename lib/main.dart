de281459c6da16e4835eb4ef7459368df54bf358
Armiron Kurbalaj <armironkurbalaj@gmail.com>
2023-03-08 23:55:10 +0100

    First Commit with navigation

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import './categories_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Meals',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        accentColor: Colors.redAccent,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+            bodyLarge: TextStyle(
+              color: Color.fromRGBO(20, 51, 51, 1),
+            ),
+            bodyMedium: TextStyle(
+              color: Color.fromRGBO(20, 51, 51, 1),
+            ),
+            titleMedium: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'RobotoCondensed',
+            )),
+      ),
+      home: CategoriesScreen(),
+    );
+  }
+}
